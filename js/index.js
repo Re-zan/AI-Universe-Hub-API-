154f8ea60a08912f224b96453a5d0de3cc1bef12
Re-zan <zannat881all@gmail.com>
2023-03-03 21:27:58 +0600

    date title and icon added

--- js/index.js
+++ js/index.js
@@ -18,10 +18,21 @@ const getData = (datas) => {
           />
         </figure>
         <div class="card-body">
-          <h2 class="card-title">Features</h2>
+          <h2 class="card-title font-semibold text-2xl">Features</h2>
         <div id="fet_content"></div>
-          <div class="card-actions ">
-            <button class="btn btn-primary">Buy Now</button>
+          <div class="card-actions boder-t flex justify-between  ">
+          <div>
+            <h2 class="font-semibold text-2xl">${data.name}</h2>
+            <div class="pt-3 text-[#585858] text-base">
+              <i class="fa-solid fa-calendar-days"> <span class="pl-1.5">${data.published_in}</span> </i>
+            </div>
+          </div>
+          <div class="mt-5">
+            <i
+              class="fa-solid fa-arrow-right bg-[#FEF7F7] text-[#EB5757] p-3 rounded-full "
+            ></i>
+          </div>
+        
           </div>
         </div>
       </div>
